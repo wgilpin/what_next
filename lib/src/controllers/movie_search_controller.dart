1855b8241ae2328761d535ee3d5f252bd46430b8
Will Gilpin <wgilpin@gmail.com>
2022-11-07 00:13:49 +0000

    order movie search by exact then by year

--- lib/src/controllers/movie_search_controller.dart
+++ lib/src/controllers/movie_search_controller.dart
@@ -18,8 +18,22 @@ class MovieSearchController extends GetxController {
       print('url: $url');
       final response = await _dio.get(url);
       final results = List<Map<String, dynamic>>.from(response.data['results']);
-      searchResults.value =
+
+      // order results by exact match then byyear
+      var interimResults =
           results.map((movieData) => Movie.fromMap(movieData)).toList();
+      List<Movie> exactMatches = interimResults
+          .where((m) => m.title.toLowerCase() == text.toLowerCase())
+          .toList();
+      List<Movie> otherMatches = interimResults
+          .where((m) => m.title.toLowerCase() != text.toLowerCase())
+          .toList();
+      otherMatches.sort((a, b) => b.year.compareTo(a.year));
+      searchResults.value = exactMatches + otherMatches;
+
+      // update getX state
+      update();
+
       print('found ${searchResults.length} movies');
     } on DioError catch (dioError) {
       throw MoviesException.fromDioError(dioError);
