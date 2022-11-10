aa7a7291f62e3b0bf8358d9d2a4581c96edb25ba
Will Gilpin <wgilpin@gmail.com>
2022-11-10 19:13:02 +0000

    set up app bindings

--- lib/main.dart
+++ lib/main.dart
@@ -29,24 +29,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
         title: 'What Next',
-        initialBinding: AuthBinding(),
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          // Define the default brightness and colors.
-          brightness: Brightness.dark,
-          primaryColor: Colors.lightBlue[800],
-
-          // Define the default font family.
-          fontFamily: 'Georgia',
-
-          // Define the default `TextTheme`. Use this to specify the default
-          // text styling for headlines, titles, bodies of text, and more.
-          textTheme: const TextTheme(
-            headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-            headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-            bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-          ),
-        ),
+        initialBinding: AppBindings(),
+        theme: makeTheme(),
         home: Root());
+  }
+
+  ThemeData makeTheme() {
+    return ThemeData(
+      primarySwatch: Colors.blue,
+      // Define the default brightness and colors.
+      brightness: Brightness.dark,
+      // scaffoldBackgroundColor: Colors.grey[200],
+
+      // Define the default font family.
+      fontFamily: 'Georgia',
+
+      // Define the default `TextTheme`. Use this to specify the default
+      // text styling for headlines, titles, bodies of text, and more.
+      textTheme: const TextTheme(
+        headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+        headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+        bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+      ),
+    );
   }
 }
