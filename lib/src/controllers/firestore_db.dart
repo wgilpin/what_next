1cf10a08b115a74f5ea72618561e5ada397a2772
Will Gilpin <wgilpin@gmail.com>
2022-11-12 22:10:39 +0000

    find average rating for movies

--- lib/src/controllers/firestore_db.dart
+++ lib/src/controllers/firestore_db.dart
@@ -1,14 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:what_next/src/controllers/auth_controller.dart';
 import 'package:what_next/src/models/movie.dart';
 import 'package:what_next/src/models/review.dart';
+import "package:collection/collection.dart";
 
 class FirestoreDB {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Stream<List<Review>> reviewsForUserStream(String uid) {
-    print('in reviewsForUserStream');
+    debugPrint('in reviewsForUserStream');
     return _firestore
         .collectionGroup('reviews')
         .where('user', isEqualTo: uid)
@@ -19,26 +21,26 @@ class FirestoreDB {
         final reviewModel = Review.fromSnapshot(review);
         reviews.add(reviewModel);
       }
-      print('review stream:${reviews.length}');
+
+      debugPrint('review stream:${reviews.length}');
       return reviews;
     });
   }
 
   Stream<List<Review>> recommendsStream(String uid) {
-    print('in recommendsStream');
+    debugPrint('in recommendsStream');
     return _firestore
-        .collection('reviews')
-        .where('user', isNotEqualTo: uid)
-        .orderBy('user')
+        .collectionGroup('reviews')
         .orderBy('when', descending: true)
         .snapshots()
         .map((QuerySnapshot query) {
-      List<Review> reviews = [];
+      List<Review> rawReviews = [];
       for (var review in query.docs) {
         final reviewModel = Review.fromSnapshot(review);
-        reviews.add(reviewModel);
+        rawReviews.add(reviewModel);
       }
-      print('recommends stream:${reviews.length}');
+      var reviews = processReviews(rawReviews: rawReviews, user: uid);
+      debugPrint('recommends stream:${reviews.length}');
       return reviews;
     });
   }
@@ -65,5 +67,27 @@ class FirestoreDB {
         .doc(review.movieId)
         .collection('reviews')
         .add(review.toMap());
+  }
+
+  // remove duplicate reviews and work out average ratings
+  List<Review> processReviews(
+      {required List<Review> rawReviews, required String user}) {
+    List<Review> results = [];
+    var groups = groupBy(rawReviews, (Review r) => r.movieId);
+    for (var id in groups.keys) {
+      var reviews = groups[id]!;
+      // ignore any group if I am the only reviewer
+      if (reviews.length == 1 && reviews[0].user == user) {
+        continue;
+      }
+      var rating = reviews
+              .map((r) => r.rating)
+              .fold<double>(0.0, (acc, val) => acc + val) /
+          reviews.length;
+      var first = reviews.first;
+      first.rating = rating;
+      results.add(first);
+    }
+    return results;
   }
 }
