e2c127889684e0349025117d5fab8d820a972b90
Will Gilpin <wgilpin@gmail.com>
2022-11-14 13:29:35 +0000

    added page for friend management

---
+++ lib/src/models/user_profile.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+
+// https://github.com/kudpig/flutter_api_json_sample_at_movie/blob/main/lib/home/movie.dart
+
+class UserProfile {
+  String displayName;
+  bool active;
+  DateTime lastActive;
+  String id;
+
+  UserProfile({
+    required this.displayName,
+    required this.active,
+    required this.lastActive,
+    required this.id,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'display_name': displayName,
+      'active': active,
+      'last_active': lastActive,
+      'id': id,
+    };
+  }
+
+  factory UserProfile.fromMap(Map<String, dynamic> map) {
+    return UserProfile(
+      displayName: map['display_name'] ?? 'Unnamed',
+      active: map['active'] ?? false,
+      lastActive: map['last_active'] ?? DateTime.now(),
+      id: map['id'].toString(),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserProfile.fromJson(String source) => UserProfile.fromMap(json.decode(source));
+
+  static UserProfile fromSnapshot(DocumentSnapshot snapshot) {
+    var map = snapshot.data() as Map<String, dynamic>;
+    return UserProfile(
+      displayName: map['display_name'] ?? 'Unnamed',
+      active: map['active'] ?? false,
+      lastActive: map['last_active'] ?? DateTime.now(),
+      id: snapshot.id,
+    );
+  }
+}
---
+++ lib/src/views/friends_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:what_next/src/models/user_profile.dart';
+import 'package:what_next/src/theme.dart';
+import 'package:what_next/src/views/drawer.dart';
+
+class FriendsPage extends StatelessWidget {
+  FriendsPage({super.key});
+  final emailAddress = ''.obs;
+  final emailController = TextEditingController();
+  RxList<UserProfile> friends = RxList<UserProfile>([]);
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: makeTheme(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('What Next'),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  onChanged: (value) => emailAddress.value = value,
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.search),
+                    hintText: "Your friend's email address",
+                    labelText: 'Find a friend',
+                  ),
+                  validator: (String? value) {
+                    return (value == null || GetUtils.isEmail(value))
+                        ? 'Please enter a valid email address'
+                        : null;
+                  },
+                ),
+                Obx(() => GetUtils.isEmail(emailAddress.value)
+                    ? Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Invite'),
+                        ),
+                      )
+                    : Text('')),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text('Friends', style: Theme.of(context).textTheme.headline6),
+                if (friends.isEmpty)
+                  const Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                    child: Text(
+                        'You have no friends yet. Type an email address to invite them'),
+                  )
+                else
+                  Expanded(
+                      child: ListView.builder(
+                    itemCount: friends.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: const Icon(Icons.person),
+                        title: Text(friends[index].displayName),
+                        subtitle: Text(friends[index].lastActive.toString()),
+                      );
+                    },
+                  ))
+              ],
+            ),
+          ),
+        ),
+        drawer: getDrawer(),
+      ),
+    );
+  }
+}
