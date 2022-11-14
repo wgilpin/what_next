551d173fd62cec41cf3238f4fa69289f74746819
Will Gilpin <wgilpin@gmail.com>
2022-11-14 13:29:03 +0000

    moved theme to its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/gestures.dart';
 import 'package:get/get.dart';
-import 'package:what_next/src/controllers/auth_controller.dart';
 import 'package:what_next/src/controllers/bindings/auth_binding.dart';
+import 'package:what_next/src/theme.dart';
 import 'package:what_next/src/utils/root.dart';
-import 'package:what_next/src/views/login/login_page.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
@@ -34,27 +33,6 @@ class MyApp extends StatelessWidget {
         theme: makeTheme(),
         scrollBehavior: MyCustomScrollBehavior(),
         home: Root());
-  }
-
-
-  ThemeData makeTheme() {
-    return ThemeData(
-      primarySwatch: Colors.blue,
-      // Define the default brightness and colors.
-      brightness: Brightness.dark,
-      // scaffoldBackgroundColor: Colors.grey[200],
-
-      // Define the default font family.
-      fontFamily: 'Georgia',
-
-      // Define the default `TextTheme`. Use this to specify the default
-      // text styling for headlines, titles, bodies of text, and more.
-      textTheme: const TextTheme(
-        headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-        headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-        bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-      ),
-    );
   }
 }
 
