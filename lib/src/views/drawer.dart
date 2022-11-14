b2f7fe78786da8a354423abed611e6073f6e635b
Will Gilpin <wgilpin@gmail.com>
2022-11-14 13:30:04 +0000

    layout improvemts and friend page link

--- lib/src/views/drawer.dart
+++ lib/src/views/drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:what_next/src/controllers/auth_controller.dart';
+import 'package:what_next/src/views/friends_page.dart';
 import 'package:what_next/src/views/login/login_page.dart';
 import 'package:what_next/src/views/recommend/recommends_page.dart';
 
@@ -9,35 +10,53 @@ import 'my_movies/my_movies_page.dart';
 Drawer getDrawer() {
   var controller = Get.find<AuthCtl>();
   return Drawer(
-    child: ListView(
-      // Important: Remove any padding from the ListView.
-      padding: EdgeInsets.zero,
+    child: Column(
       children: [
-        DrawerHeader(
-          decoration: const BoxDecoration(
-            color: Colors.blue,
+        ListView(
+          shrinkWrap: true,
+          children: [
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(controller.user?.displayName ?? 'User'),
+            ),
+            ListTile(
+              title: const Text('Recomended'),
+              leading: const Icon(Icons.recommend),
+              onTap: () {
+                Get.to(RecommendsPage());
+              },
+            ),
+            ListTile(
+              title: const Text('My Shows'),
+              leading: const Icon(Icons.movie),
+              onTap: () {
+                Get.to(const MyMoviesPage());
+              },
+            ),
+            ListTile(
+              title: const Text('Friends'),
+              leading: const Icon(Icons.groups),
+              onTap: () {
+                Get.to(FriendsPage());
+              },
+            ),
+          ],
+        ),
+        Expanded(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: ListTile(
+              title: const Text('Log out'),
+              leading: const Icon(Icons.logout),
+              onTap: () async {
+                var controller = Get.find<AuthCtl>();
+                await controller.signOut();
+                Get.to(LoginPage());
+              },
+            ),
           ),
-          child: Text(controller.user?.displayName ?? 'User'),
-        ),
-        ListTile(
-          title: const Text('Recomended'),
-          onTap: () {
-            Get.to(RecommendsPage());
-          },
-        ),
-        ListTile(
-          title: const Text('My Shows'),
-          onTap: () {
-            Get.to(const MyMoviesPage());
-          },
-        ),
-        ListTile(
-          title: const Text('Log out'),
-          onTap: () async {
-            var controller = Get.find<AuthCtl>();
-            await controller.signOut();
-            Get.to(LoginPage());
-          },
         ),
       ],
     ),
