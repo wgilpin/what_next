8999fe74a40cc7923a7acc651fa121a7d7fb0281
Will Gilpin <wgilpin@gmail.com>
2022-11-22 18:53:22 +0000

    init the reviews controller on start

--- lib/src/controllers/bindings/auth_binding.dart
+++ lib/src/controllers/bindings/auth_binding.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
 import 'package:what_next/src/controllers/auth_controller.dart';
+import 'package:what_next/src/controllers/reviews_controller.dart';
 
 class AppBindings extends Bindings {
   @override
   void dependencies() {
     Get.put(AuthCtl());
+    Get.put(ReviewsCtl());
   }
 }
