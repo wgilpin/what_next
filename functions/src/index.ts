e5fab55f9e762b18a589559be0fd564d932e17f4
Will Gilpin <wgilpin@gmail.com>
2022-11-22 13:18:19 +0000

    delete user data when their account is deleted

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,10 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import {firestore} from "firebase-admin";
 
 admin.initializeApp();
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions
-    .region("europe-west1")
-    .https.onRequest((request, response) => {
-      functions.logger.info("Hello logs!", {structuredData: true});
-      response.send("Hello from Firebase!");
-    });
+// https://firebase.google.com/docs/functions/typescript
 
 export const newUser = functions
     .region("europe-west1")
@@ -21,6 +14,23 @@ export const newUser = functions
         display_name: user.displayName?.toLowerCase(),
       });
       functions.logger.info("New user created", {structuredData: true});
+      functions.logger.info(user); // log the user object
+      return null;
+    });
+
+export const removeUser = functions
+    .region("europe-west1")
+    .auth.user()
+    .onDelete(async (user) => {
+      firestore().collection("users").doc(user.uid).delete();
+      const docs = await firestore()
+          .collectionGroup("reviews")
+          .where("user", "==", user.uid)
+          .get();
+      docs.forEach((element) => {
+        element.ref.delete();
+      });
+      functions.logger.info("User deleted", {structuredData: true});
       functions.logger.info(user); // log the user object
       return null;
     });
