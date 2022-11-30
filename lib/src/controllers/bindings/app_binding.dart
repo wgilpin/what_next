54d7be3e39dcd3a57ec3beca322eea024759b748
Will Gilpin <wgilpin@gmail.com>
2022-11-30 23:34:04 +0000

    fix: put all controllers upfront

--- lib/src/controllers/bindings/app_binding.dart
+++ lib/src/controllers/bindings/app_binding.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:what_next/src/controllers/auth_controller.dart';
+import 'package:what_next/src/controllers/genre_controller.dart';
+import 'package:what_next/src/controllers/my_reviews_controller.dart';
 import 'package:what_next/src/controllers/reviews_controller.dart';
 import 'package:what_next/src/controllers/service_controller.dart';
 
@@ -11,7 +13,10 @@ class AppBindings extends Bindings {
   void dependencies() {
     Get.put(AuthCtl(FirebaseAuth.instance));
     Get.put(ReviewsCtl());
+    Get.put(MyReviewsCtl());
     Get.put(ServiceCtl());
+    Get.put(GenreCtl());
+
     debugPrint('Bindings set');
   }
 }
